7a1fd2073c4926e09428f02a94b045e2ac3e6f65
Dmitriy Dubson <d.dubson@gmail.com>
2017-08-19 23:15:25 -0400

    Added file list view

--- src/main/kotlin/com/ddubson/hl7/files/HL7FileCounter.kt
+++ src/main/kotlin/com/ddubson/hl7/files/HL7FileCounter.kt
@@ -2,11 +2,13 @@ package com.ddubson.hl7.files
 
 import com.ddubson.hl7.views.RootPaneController
 import org.springframework.messaging.Message
+import java.io.File
 
 class HL7FileCounter(private val rootPaneController: RootPaneController) {
     private var counter = 0
 
-    fun count(msg: Message<*>) {
+    fun count(msg: Message<File>) {
         rootPaneController.incFilesRead(++counter)
+        rootPaneController.addFileNameToFileList(msg.payload.name)
     }
 }
--- src/main/kotlin/com/ddubson/hl7/views/RootPaneController.kt
+++ src/main/kotlin/com/ddubson/hl7/views/RootPaneController.kt
@@ -1,17 +1,28 @@
 package com.ddubson.hl7.views
 
+import javafx.application.Platform
+import javafx.collections.ObservableList
 import javafx.fxml.FXML
+import javafx.fxml.Initializable
+import javafx.scene.control.ListView
 import javafx.scene.control.TextArea
 import javafx.scene.text.Text
 import org.springframework.stereotype.Component
+import java.net.URL
+import java.util.*
 
 @Component
-class RootPaneController {
+class RootPaneController : Initializable {
+    private var list : ObservableList<String>? = null
+
     @FXML
     private var logField: TextArea? = null
 
     @FXML
     private var filesReadText: Text? = null
+
+    @FXML
+    private var fileList: ListView<String>? = null
 
     fun logToScreen(message: String) {
         logField!!.text = "${logField!!.text}\n$message"
@@ -19,5 +30,13 @@ class RootPaneController {
 
     fun incFilesRead(count: Int) {
         filesReadText!!.text = count.toString()
+    }
+
+    fun addFileNameToFileList(fileName: String) {
+        Platform.runLater({ -> list!!.add(fileName)})
+    }
+
+    override fun initialize(location: URL?, resources: ResourceBundle?) {
+        list = fileList!!.items
     }
 }
